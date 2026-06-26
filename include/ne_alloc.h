7a6c4d4155fa8133585e52edac0ef4b3d88361b0
Hamza Lahmimsi <hlahmimsi@gmail.com>
2026-06-26 11:53:53 -0400

    Track live count in pool allocator

--- include/ne_alloc.h
+++ include/ne_alloc.h
@@ -2,6 +2,7 @@
 #define NE_ALLOC_H
 
 #include <stdint.h>
+#include <stddef.h>
 
 uint32_t ne_pool_alloc(void **pool_ptr, uint32_t *count_ptr, uint32_t *cap_ptr, size_t slot_size);
 
--- src/ne_alloc.c
+++ src/ne_alloc.c
@@ -18,6 +18,7 @@ uint32_t ne_pool_alloc(void **pool_ptr, uint32_t *count_ptr, uint32_t *cap_ptr, 
     for (uint32_t i = 0; i < cap; i++) {
         bool *occupied = (bool *)(pool + i * slot_size);
         if (!*occupied) {
+            *count_ptr = *count_ptr + 1;
             return i;
         }
     }
